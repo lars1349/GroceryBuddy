edd291b4f381b488af1396e99e3f086f1f1ed45d
larsmostue <mostuelars@gmail.com>
2025-03-25 11:07:38 +0100

    a

--- 4-b.Edit_history_model_handleliste_model.js
+++ 4-b.Edit_history_model_handleliste_model.js
@@ -1,1 +1,16 @@
-// test
+const model = {
+    app: {
+        currentPage: 'editShoppingList'
+    },
+    groceryInputs: {
+        editGroceryItems: [
+        { name: 'Bananer', id: 1, },
+        { name: 'Epler', id: 2, },
+        { name: 'Cola', id: 3, },
+        { name: 'Sjokolade', id: 4, },
+        { name: 'Melk', id: 5, },
+        { name: 'Egg', id: 6, },
+        { name: 'Pølser', id: 7, },
+        ]
+    },
+}
