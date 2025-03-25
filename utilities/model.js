56a064d2f21796bad67c0c6b3af91eea7d8f1070
goitomdev <goitomdev@gmail.com>
2025-03-25 11:34:18 +0000

    commit

---
+++ utilities/model.js
@@ -0,0 +1,73 @@
+const model = {
+    app: {
+        currentPage: 'CreateNewShoppingList', 
+
+        pages: [
+            "login",
+            "overviewShoppingLists",
+            "createNewShoppingList",
+            "profile",
+        ],
+    },
+
+    inputs: {
+        login: {
+            email: '',
+            password: '',
+        },
+
+        overviewShoppingLists: {
+            selectedListId: '',  // myList: ''
+            
+        },
+
+        newShoppingList: {
+            name: '',
+            sharedWith: [],  // List of user IDs 
+            confirm: false,
+            
+        },
+
+        
+    },
+
+    data: {
+        users: [
+            {
+                id: 1,
+                name: 'Rebecka',
+                email: 'rebecka@team3.no',
+                password: '*****',
+                
+            },
+            {
+                id: 2,
+                name: 'Rebecka2',
+                email: 'rebecka2@team3.no',
+                password: '*****',
+                
+            }
+        ],
+
+        shoppingLists: [
+            { id: 1, name: "Weekly Groceries", ownerId: 1, sharedWith: [2] },
+            { id: 2, name: "Birthday Party Supplies", ownerId: 2, sharedWith: [1] },
+        ],
+
+        shoppingListItems: [
+            { id: 1, listId: 1, productId: 1, quantity: 2 },
+            { id: 2, listId: 1, productId: 2, quantity: 1 },
+            { id: 3, listId: 2, productId: 3, quantity: 5 },
+        ],
+
+        products: [
+            { id: 1, name: "Milk" },
+            { id: 2, name: "Bread" },
+            { id: 3, name: "Balloons" },
+        ],
+
+        profile: [   
+         { id: 2, userId: 1, status: "logged_out" },
+        ],
+    },
+};
