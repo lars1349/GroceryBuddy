a11f8de5fe24de18065415fe7a54e4d56b7348e6
goitomdev <goitomdev@gmail.com>
2025-03-26 15:18:58 +0000

    commit

--- views/mainView.js
+++ views/mainView.js
@@ -1,8 +1,3 @@
-// function mainView(){
-//     document.getElementById("app").innerHTML = /*HTML*/ `<h1> Team 3 Superduper Handleliste app  </h1>`;
-
-// }
-
 function mainView(){
     document.getElementById('app').innerHTML= /*HTML*/ `<h1> Team 3 Superduper Handleliste App </h1>`;
 
