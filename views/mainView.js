bcae005583d9407495ab28776d8775c653d6ebb2
larsmostue <mostuelars@gmail.com>
2025-04-01 11:27:33 +0200

    commit

--- views/mainView.js
+++ views/mainView.js
@@ -35,6 +35,9 @@ function updateView() {
         case 'settings':
             currentView =shoppingListsSettingsView();
             break;
+        case 'createUser':
+            currentView = createNewUserView();
+            break;
         } 
                 
 
