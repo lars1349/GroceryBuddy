e06f11fa5645eec984545e1d245376cd2f08295c
larsmostue <mostuelars@gmail.com>
2025-04-25 11:30:54 +0200

    commit

--- views/historyView.js
+++ views/historyView.js
@@ -15,7 +15,6 @@ function historyView() {
                 onclick="openShoppingList(${list.id})">
                 ${list.name} <br />
                 <small>Fullført: ${list.completedDate}</small>
-                <button onclick="toggleListActive(${list.id}, true)">Gjør aktiv</button>
             </div>
         `;
     }
