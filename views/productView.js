c7572c92614baed8aaacef141c2f3597a6b3a2b5
goitomdev <goitomdev@gmail.com>
2025-04-30 09:35:17 +0100

    commit

--- views/productView.js
+++ views/productView.js
@@ -15,7 +15,9 @@ function productView() {
         
             <h2>${listName}</h2>
             
-            
+            <div id="saveStatus" style="margin-bottom: 10px; color: ${model.app.isSaving ? 'red' : 'green'};">
+                ${model.app.isSaving ? 'Lagrer...' : 'Alt er lagret ✅'}
+            </div> 
             <button class="btn products" onclick="toggleProductView()">Legg til varer</button>
             <button class="btn products" onclick="goToSettings()">Rediger liste</button>
         
@@ -41,9 +43,7 @@ function productView() {
     `;
 }
 
-{/* <div id="saveStatus" style="margin-bottom: 10px; color: ${model.app.isSaving ? 'red' : 'green'};">
-                ${model.app.isSaving ? 'Lagrer...' : 'Alt er lagret ✅'}
-            </div> */}
+
 
 
 function createProductTableRows() {
